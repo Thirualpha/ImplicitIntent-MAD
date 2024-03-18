7cedb7ab430186a7f89daf5aba5d7db2a29fa3c2
Thirualpha <113031702+Thirualpha@users.noreply.github.com>
2024-03-18 23:52:46 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -1,14 +1,38 @@
-package com.example.implicitintent;
+package com.example.implicit_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        EditText editText;
+        Button button;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Intent intent=new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("https://www.geeksforgeeks.org/"));
+//        startActivity(intent);
+        button = findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editTextText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url=editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
     }
 }
